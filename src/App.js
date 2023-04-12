4bd4db144803d503f6c62c8d6748f3e6dd52cec7
Ashish <ajerath@innow8apps.com>
2023-04-12 18:07:45 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ function App() {
     <div className="opt-top-container">
       <div className="header">
         <div className="header-inner">
-          <div className="logo">
-            <LogoSvg />
+          <div className="logo-div">
+            <LogoSvg className="logo" />
           </div>
-          <div>
+          <div className="header-items">
             <h2>Outside Team Building</h2>
             <h2>Challenges</h2>
             <h2>Team Reports</h2>
             <h2>Invite</h2>
             <h2>CreightonCreighton</h2>
           </div>
+        </div>
+      </div>
+      <div className="bottom-content">
+        <div className="bottom-inner">
+          <h2 className="text-1">{`Activity Library > Outside Team Building`}</h2>
+          <h2 className="text-2">Outside Team Building</h2>
+          <h2 className="text-3">
+            Record, manage, and track all of your team building activities.
+          </h2>
         </div>
       </div>
     </div>
